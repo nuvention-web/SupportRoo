9061b31fdb4fea5cd17a631d773f8584dbb0d43d
avyfain <avyfain@gmail.com>
2015-04-08 15:07:34 -0500

    basic show board page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!
+
   def show
+    boards = current_user.boards.includes(:supporters)
+    @own_boards = boards.where(:supporters => {owner: true})
+    @support_boards = boards.where(:supporters => {owner: nil})
   end
 end
