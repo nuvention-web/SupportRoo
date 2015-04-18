4a2e562597a93ffe5dc431054770cc51872f9bc9
avyfain <avyfain@gmail.com>
2015-04-18 13:15:08 -0500

    revamp homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  
+  def flash_helper name
+    n = name.to_s
+    if n == 'notice' 
+      'success' 
+    elsif n == 'warning'
+      'warning'
+    else
+      'alert' 
+    end
+  end
+
 end
