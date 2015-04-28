de0e2481744e055466d22467776d967225459b34
avyfain <avyfain@gmail.com>
2015-04-28 11:33:20 -0500

    fixes names on emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,7 @@ class UserMailer < ApplicationMailer
 	def notify_supporter(task)
 		@task = task
     @supporter = task.user
-    @owner = task.board.owners.first
+    @owner = task.board.creator
 		mail to: @supporter.email, subject: "[SupportRoo] Thank you for your Support!"
 	end
 
@@ -11,28 +11,28 @@ class UserMailer < ApplicationMailer
 		@task = task
     owner_emails = @task.board.owner_emails
     @volunteer = @task.user
-    @owner = @task.board.owners.first
-		mail to: owner_emails, subject: "[SupportRoo] #{@volunteer} just signed up for a task on your board!"
+    @owner = @task.board.creator
+		mail to: owner_emails, subject: "[SupportRoo] #{@volunteer.first_name} #{@volunteer.last_name} just signed up for a task on your board!"
 	end
 
   def notify_supporter_of_edit(task)
     @task = task
     @supporter = task.user
-    @owner = task.board.owners.first
-    mail to: @supporter.email, subject: "[SupportRoo] A task you signed up for on #{@owner}'s board was updated!"
+    @owner = task.board.creator
+    mail to: @supporter.email, subject: "[SupportRoo] A task you signed up for on #{@owner.first_name} #{@owner.last_name}'s board was updated!"
   end
 
   def invite_user(invite)
     @invite = invite
-    @owner = @invite.board.owners.first
-    mail to: @invite.email, subject: "[SupportRoo] You have been invited to #{@owner}'s Support Board!"
+    @owner = @invite.board.creator
+    mail to: @invite.email, subject: "[SupportRoo] You have been invited to #{@owner.first_name} #{@owner.last_name}'s Support Board!"
   end
 
   def notify_owners_of_completion(task)
     @task = task
     owner_emails = @task.board.owner_emails
     @volunteer = @task.user
-    @owner = @task.board.owners.first
-    mail to: owner_emails, subject: "[SupportRoo] Your task on #{@owner}'s board, #{@task.title}, has been completed!"
+    @owner = @task.board.creator
+    mail to: owner_emails, subject: "[SupportRoo] Your task on #{@owner.first_name} #{@owner.last_name}'s board, #{@task.title}, has been completed!"
   end
 end
