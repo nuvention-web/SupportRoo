b2350e164160ef2c1ccceb6b09e631c19bc18885
avyfain <avyfain@gmail.com>
2015-06-07 16:00:11 -0500

    add custom errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,18 @@
 Rails.application.routes.draw do
 
+  get 'errors/file_not_found'
+
+  get 'errors/unprocessable'
+
+  get 'errors/internal_server_error'
+
   root 'welcome#index'
   get 'welcome/index'
+  
+  match '/404', to: 'errors#file_not_found', via: :all
+  match '/422', to: 'errors#unprocessable', via: :all
+  match '/500', to: 'errors#internal_server_error', via: :all
+
 
   get 'users/show'
   devise_for :users
